af9332f2f4aa229964c6fe9b2f6c7cf205471ae8
jjrzov <jjrzov@gmail.com>
2026-07-07 03:46:50 +0000

    Can create base tree from AND/ORs

--- ros_ws/src/basic_trees/basic_trees/Goals/goal_tree.py
+++ ros_ws/src/basic_trees/basic_trees/Goals/goal_tree.py
@@ -37,19 +37,12 @@ def getAction(action_str, action_database):
     return TestAction(name=action_str, action_database=action_database)
 
 
-def runTree(init_state, goal_state, action_database, traverse=BFS(), scorer=None):
-    # Create the goal tree
-    goal_root = buildGoalTree(goal_state)
-    goal_tree = py_trees.trees.BehaviourTree(
-        goal_root=goal_root,
+def runTree(init_state, goal_term, action_database, traverse=BFS(), scorer=None):
+    # Create the base tree from the goal
+    root = buildBaseTree(goal_term)
+    tree = py_trees.trees.BehaviourTree(
+        root=root,
     )
-
-    # Create the tree to perform expand on
-    expansion_root = createRoot(goal_state)
-    expansion_tree = py_trees.trees.BehaviourTree(
-        expansion_root=expansion_root,
-    )
-
 
     # Initialise the blackboard BEFORE setting up the tree
     blackboard = py_trees.blackboard.Client(name="Init")
@@ -57,31 +50,26 @@ def runTree(init_state, goal_state, action_database, traverse=BFS(), scorer=None
     setupWorld(blackboard, init_state) # Define world literals
 
     # Set up the tree
-    goal_tree.setup()
-    expansion_tree.setup()
+    tree.setup()
 
-    py_trees.display.render_dot_tree(goal_root, name=f"goal_tree")
-
-    
     # traverse = DFS()            # EDIT traversal function here
     # scorer = ConditionCompletionScorer(Action_Database)     # EDIT cost metric for adding actions in expand here
 
     expanded_literals = set()
 
-    while goal_root.status != py_trees.common.Status.SUCCESS:
+    while root.status != py_trees.common.Status.SUCCESS:
         # Handle tree returning RUNNING or FAILURE
-        goal_tree.tick()
-        expansion_tree.tick()
+        tree.tick()
 
 
         print(f"--- tick ---")
-        print(f"status: {goal_root.status}")
+        print(f"status: {root.status}")
         print(f"world_state: {blackboard.world_state}")
 
 
-        if goal_root.status == py_trees.common.Status.FAILURE:
+        if root.status == py_trees.common.Status.FAILURE:
             # Expand when tree returns failure
-            next_condition = traverse.getNextCondition(expansion_root, expanded_literals)
+            next_condition = traverse.getNextCondition(root, expanded_literals)
 
             if next_condition == None:
                 print("No more conditions to expand - unsolvable")
@@ -92,25 +80,20 @@ def runTree(init_state, goal_state, action_database, traverse=BFS(), scorer=None
             # Add condition literals to expanded set
             expanded_literals.add(frozenset(next_condition.preconditions))  # Needs to be frozen to keep literals grouped as conditions
             
-            expansion_root = expand(expansion_root, next_condition, action_database, getAction, scorer)
-            prune(expansion_root, expanded_literals)  # Remove sequence structures that have already been expanded elsewhere
-            expansion_tree.root = expansion_root
+            root = expand(root, next_condition, action_database, getAction, scorer)
+            prune(root, expanded_literals)  # Remove sequence structures that have already been expanded elsewhere
+            tree.root = root
 
-    py_trees.display.render_dot_tree(expansion_root, name=f"test_tree {counter}")
-    return expansion_root
+    py_trees.display.render_dot_tree(root, name=f"test_tree {counter}")
+    return root
 
 
-# def main():
-#     root = runTree(states_database[0].copy(), states_database[-1], action_database, 
-#                    DFS(), ConditionCompletionScorer(action_database))
+def main():
+    eq = AND('a', AND('b', AND('c', AND('d', OR('e', 'f')))))
 
-#     counter += 1
-    
-#     # Record Metrics for solved trees
-#     if paper_root: 
-#         getNodeCount(paper_root)
-#     if root:
-#         getNodeCount(root)
+    tree = buildBaseTree(eq)
+    py_trees.display.render_dot_tree(tree, name=f"Goal_Base_Tree")
 
-# if __name__ == '__main__':
-#     main()
+
+if __name__ == '__main__':
+    main()
--- ros_ws/src/basic_trees/basic_trees/Goals/goal_types.py
+++ ros_ws/src/basic_trees/basic_trees/Goals/goal_types.py
@@ -10,21 +10,73 @@ class OR:
     def __init__(self, *args):
         self.children = list(args)
 
+class GoalCondition(Condition):
+    def __init__(self, preconditions={}):
+        super().__init__(preconditions=preconditions)
 
-def buildGoalTree(terms):
+
+def buildGoalTree(term):
     # Build the goal tree from the input interface
     children = []
-    for child in terms.children:
+    for child in term.children:
         if isinstance(child, (AND, OR)):
             children.append(buildGoalTree(child))
         else:
             # String as input
             children.append(Condition(name=child, preconditions={child}))
 
-    if isinstance(terms, AND):
+    if isinstance(term, AND):
         root = py_trees.composites.Sequence(name="Seq", memory=False)
-    elif isinstance(terms, OR):
+    elif isinstance(term, OR):
         root = py_trees.composites.Selector(name="FB", memory=False)
 
-    root.add_children([children])
+    root.add_children(children)
     return root
+
+
+def flatten(root):
+    # Flatten multiple operations of the same type into a list of literals
+    literals = []
+
+    for child in root.children:
+        if isinstance(child, str):
+            literals.append(child)  # String type means direct literal
+        elif isinstance(child, type(root)):
+            # Child is same type as parent, recurseviely expand
+            literals.extend(flatten(child))
+        else:
+            # Child and term have opposite types (one AND the other OR)
+            literals.append(child)
+    
+    return literals
+
+
+def buildBaseTree(term):
+    # Build the initial tree shape based on the goal
+    if isinstance(term, str):
+        return Condition(name=term, preconditions={term})
+    else:
+        # Instance of AND or OR
+        res = flatten(term)
+        if all(isinstance(item, str) for item in res) and isinstance(term, AND):
+            # All the items are literals for an AND operation
+            name = " & ".join(sorted(res))
+            return Condition(name=name, preconditions=res)  # One condition with all literals
+        else:
+            if isinstance(term, AND):
+                root = py_trees.composites.Sequence(name="Seq", memory=False)
+            else:
+                root = py_trees.composites.Selector(name="FB", memory=False)
+
+            for item in res:                                # TODO: NO ORDER IN HOW LITERALS ARE PLACED, COULD MATTER FOR SEQUENCE OF AND TERMS like: AND(1st, 2nd)
+                if isinstance(item, str):
+                    child = Condition(name=item, preconditions={item})
+                else:
+                    # Flatten will return a list of strings and any opposite operations
+                    child = buildBaseTree(item) # Recurse to handle opposite type and branching        
+                
+                root.add_child(child)
+            return root # Return root of tree
+
+
+    
